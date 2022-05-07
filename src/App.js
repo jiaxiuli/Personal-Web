6c7db91c3d145664fea86e937124e3d02fb31900
lijiaxiu <jiaxiu_li@126.com>
2022-05-07 16:26:43 +0800

    fix: 取消对滚动条的控制

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 /*
  * @Author: 李佳修
  * @Date: 2022-05-07 13:22:23
- * @LastEditTime: 2022-05-07 16:07:35
+ * @LastEditTime: 2022-05-07 16:25:24
  * @LastEditors: 李佳修
  * @FilePath: /Personal-Web/src/App.js
  */
 import Home from "./views/Home/Home";
 
 // 取消浏览器对滚动条位置的记录
-if (window.history.scrollRestoration) {
-    window.history.scrollRestoration = 'manual';
-}
+// if (window.history.scrollRestoration) {
+//     window.history.scrollRestoration = 'manual';
+// }
 const App = () => <Home />;
 
 export default App;
